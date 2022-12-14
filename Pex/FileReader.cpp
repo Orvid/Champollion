f01dac72d7c5b5d0a02a0bf36bdd2c9b83eaca1e
nikitalita <69168929+nikitalita@users.noreply.github.com>
2022-12-14 15:11:59 -0800

    Move byteswap to seperate header

--- Pex/FileReader.cpp
+++ Pex/FileReader.cpp
@@ -1,8 +1,9 @@
 #include "FileReader.hpp"
+#include "ByteSwap.hpp"
+
 #include <iostream>
 #include <sstream>
 #include <iomanip>
-#include <type_traits>
 
 #include <cassert>
 
@@ -43,53 +44,6 @@ void Pex::FileReader::read(Pex::Binary &binary)
     read(binary.getDebugInfo());
     read(binary.getUserFlags());
     read(binary.getObjects());    
-}
-
-constexpr uint16_t _Byteswap_ushort(const uint16_t _Val) noexcept {
-    if (std::_Is_constant_evaluated()) {
-        return static_cast<unsigned short>((_Val << 8) | (_Val >> 8));
-    } else {
-        return _byteswap_ushort(_Val);
-    }
-}
-
-constexpr uint32_t _Byteswap_ulong(const uint32_t _Val) noexcept {
-    if (std::_Is_constant_evaluated()) {
-        return (_Val << 24) | ((_Val << 8) & 0x00FF'0000) | ((_Val >> 8) & 0x0000'FF00) | (_Val >> 24);
-    } else {
-        return _byteswap_ulong(_Val);
-    }
-}
-
-constexpr uint64_t _Byteswap_uint64(const uint64_t _Val) noexcept {
-    if (std::_Is_constant_evaluated()) {
-        return (_Val << 56) | ((_Val << 40) & 0x00FF'0000'0000'0000) | ((_Val << 24) & 0x0000'FF00'0000'0000)
-             | ((_Val << 8) & 0x0000'00FF'0000'0000) | ((_Val >> 8) & 0x0000'0000'FF00'0000)
-             | ((_Val >> 24) & 0x0000'0000'00FF'0000) | ((_Val >> 40) & 0x0000'0000'0000'FF00) | (_Val >> 56);
-    } else {
-        return _byteswap_uint64(_Val);
-    }
-}
-// template <typename Integer, typename = std::enable_if_t<std::is_integral<Integer>::value>>
-// constexpr uint64_t _get_sizeof_integer(Integer) noexcept {
-//     return constexpr (sizeof(Integer));
-// }
-template <class... T>
-constexpr bool always_false = false;
-
-template <typename Integer, typename = std::enable_if_t<std::is_integral<Integer>::value>>
-constexpr Integer byteswap(const Integer _Val) noexcept {
-    if constexpr (sizeof(Integer) == 1) {
-        return _Val;
-    } else if constexpr (sizeof(Integer) == 2) {
-        return static_cast<Integer>(_Byteswap_ushort(static_cast<uint16_t>(_Val)));
-    } else if constexpr (sizeof(Integer) == 4) {
-        return static_cast<Integer>(_Byteswap_ulong(static_cast<uint32_t>(_Val)));
-    } else if constexpr (sizeof(Integer) == 8) {
-        return static_cast<Integer>(_Byteswap_uint64(static_cast<uint64_t>(_Val)));
-    } else {
-        static_assert(always_false<Integer>, "Unexpected integer size");
-    }
 }
 
 /**
